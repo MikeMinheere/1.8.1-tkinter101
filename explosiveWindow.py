80965070462f59ff4fdb556ad7806e797759ec0c
Mike Minheere <99067548@mydavinci.nl>
2022-02-17 11:11:46 +0100

    explosive window working

--- explosiveWindow.py
+++ explosiveWindow.py
@@ -2,14 +2,23 @@ import tkinter
 import random
 import time
 
-a=6
+a = 50
+b = 6
 colors =['yellow','green','orange','pink','red','blue','purple']
 explosiveWindow =tkinter.Tk()
-explosiveWindow.geometry('50x100')
+explosiveWindow.title('Explosive window')
 time.sleep(2)
 def loop():
-    global colors,a
+    global colors,a,b
     explosiveWindow.configure(bg=random.choice(colors))
-    explosiveWindow.after(2000,loop)
+    if b >= 1:
+        print(b)
+        b-=1
+        explosiveWindow.geometry(str(a)+'x'+str(a))
+        a+=50
+        explosiveWindow.after(2000,loop)
+    elif b == 0:
+        print("BOOM!!!")
+        explosiveWindow.destroy()
 loop()
 explosiveWindow.mainloop()
