068a53bb539ab3f21ff378fa0248c994f77178c1
Mike Minheere <99067548@mydavinci.nl>
2022-05-17 10:07:48 +0200

    grabbelton fix

--- grabbelton.py
+++ grabbelton.py
@@ -1,13 +1,15 @@
 def buttonAction():
-    random.shuffle(grabbelton)
-    displayItem = tkinter.Label(
-        root,
-        bg = 'light green',
-        text= 'Gefeliciteerd! je hebt een \n'+grabbelton[0]+' gegrabbelt!\n\n er zijn '+str(len(grabbelton))+' items in de grabbelton' 
-    )
-    displayItem.place(relx=0.5, rely=0.51, anchor=CENTER)
-    displayItem.config(font=("Courier", 14))
-    del grabbelton[0]
+    if len(grabbelton)>0:
+        random.shuffle(grabbelton)
+        displayItem = tkinter.Label(
+            root,
+            bg = 'light green',
+            text= 'Gefeliciteerd! je hebt een \n'+grabbelton[0]+' gegrabbelt!\n\n er zijn '+str(len(grabbelton)-1)+' items in de grabbelton' 
+        )
+        displayItem.place(relx=0.5, rely=0.51, anchor=CENTER)
+        displayItem.config(font=("Courier", 14))
+        del grabbelton[0]
+    else:root.destroy()
 
 import tkinter
 from tkinter import CENTER
